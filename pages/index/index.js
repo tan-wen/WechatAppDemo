9d283a54798a01b88d0e14efe14a62df62c23be5
wentan <872701869@qq.com>
2018-01-22 16:13:58 +0800

    首次上传

---
+++ pages/index/index.js
@@ -0,0 +1,69 @@
+//index.js
+//获取应用实例
+const app = getApp()
+var color;
+Page({
+  data: {
+    motto: 'Hello World',
+    color: 'testColor',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  touchStart:function(){
+    console.log("这里是文字的点击事件开始");
+    color = "testColor-red";
+    this.setData({
+      color
+    });
+  },
+  touchEnd: function () {
+    console.log("这里是文字的点击事件结束");
+    color = "testColor";
+    this.setData({
+      color
+    });
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
